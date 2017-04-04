1eb72865290c9947f37bf58f81edbb0ed71528f3
Bruno Braga Mendonça <bruckbmrj@hotmail.com>
2017-04-04 19:43:19 -0300

    criação da matriz e etc

--- Exer06.java
+++ Exer06.java
@@ -14,5 +14,18 @@ import java.util.Scanner;
 public class Exer06 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        char[][] jogoVelha = new char[3][3];
+        
+        System.out.println("Jogador 1 = X");
+        System.out.println("Jogador 2 = O");
+        
+        boolean ganhou = false;
+        int jogada = 1;
+        
+        while(!ganhou){
+            
+        }
+                
     }
 }
