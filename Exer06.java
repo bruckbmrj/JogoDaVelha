e586cd36120dfb244b08ad532b63f131033702bb
Bruno Braga Mendonça <bruckbmrj@hotmail.com>
2017-04-05 02:19:27 -0300

    inicio de verificar vencedor

--- Exer06.java
+++ Exer06.java
@@ -12,61 +12,62 @@ import java.util.Scanner;
  * @author Bruno
  */
 public class Exer06 {
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+
         char[][] jogoVelha = new char[3][3];
-        
+
         System.out.println("Jogador 1 = X");
         System.out.println("Jogador 2 = O");
-        
+
         boolean ganhou = false;
         int jogada = 1;
         char sinal;
         int linha = 0, coluna = 0;
-        
-        while(!ganhou){
-            
+
+        while (!ganhou) {
+
             if (jogada % 2 == 1) { //jogador 1
                 System.out.println("Vez do jogador 1. Escolha linhae coluna (1-3). ");
                 sinal = 'X';
-            }else{ //jogador 2
+            } else { //jogador 2
                 System.out.println("Vez do jogador 2. Escolha linhae coluna (1-3). ");
                 sinal = 'O';
             }
             //continua a logica
             boolean linhaValida = false;
-            while(!linhaValida){
+            while (!linhaValida) {
                 System.out.println("Entre com a linha (1,2 ou 3)");
                 linha = scan.nextInt();
                 if (linha >= 1 && linha <= 3) {
                     linhaValida = true;
-                } else{
+                } else {
                     System.out.println("Entrada invalida, tente novamente.");
                 }
             }
-            
+
             boolean colunaValida = false;
             while (!colunaValida) {
                 System.out.println("Entre com a coluna (1,2 ou 3) ");
                 coluna = scan.nextInt();
                 if (coluna >= 1 && coluna <= 3) {
                     colunaValida = true;
-                } else{
+                } else {
                     System.out.println("Entrada invalida, tente novamente.");
                 }
             }
-            
+
             //cria decremento para as colunas e linhas funcionarem sem conflito
             linha--;
             coluna--;
-            if (jogoVelha[linha][coluna]=='X' || jogoVelha[linha][coluna]=='O' ) {
+            if (jogoVelha[linha][coluna] == 'X' || jogoVelha[linha][coluna] == 'O') {
                 System.out.println("Posição já usada, tente novamente.");
-            } else{ //jogada valida
+            } else { //jogada valida
                 jogoVelha[linha][coluna] = sinal;
                 jogada++;
             }
-            
+
             //imprimir tabuleiro
             for (int i = 0; i < jogoVelha.length; i++) {
                 for (int j = 0; j < jogoVelha[i].length; j++) {
@@ -74,10 +75,16 @@ public class Exer06 {
                 }
                 System.out.println();
             }
-            
+
             //verifica se há ganhador
-            
-        }
+            if ((jogoVelha[0][0] == 'X' && jogoVelha[0][1] == 'X' && jogoVelha[0][2] == 'X') //linha 1
+                    || (jogoVelha[1][0] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[1][2] == 'X') //linha 2
+                    || (jogoVelha[2][0] == 'X' && jogoVelha[2][1] == 'X' && jogoVelha[2][2] == 'X')) //linha 3
+            {
                 
+            }
+
+        }
+
     }
 }
