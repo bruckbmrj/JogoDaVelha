23297c1dd3905269b4abe4e177bab7a4b48fa928
Bruno Braga Mendonça <bruckbmrj@hotmail.com>
2017-04-18 01:35:30 -0300

    verificar se há ganhador (metodo)

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -10,32 +10,47 @@ package aula20_labs.JogoDaVelha;
  * @author Bruno
  */
 public class JogoDaVelha {
+
     char[][] jogoVelha;
     int jogada;
 
     public JogoDaVelha() {
-       char[][] jogoVelha = new char[3][3];
-       jogada = 1;
+        char[][] jogoVelha = new char[3][3];
+        jogada = 1;
     }
-    
-    
-    
-    boolean validarJogada(int linha, int coluna, char sinal){
+
+    boolean validarJogada(int linha, int coluna, char sinal) {
         if (jogoVelha[linha][coluna] == sinal || jogoVelha[linha][coluna] == 'O') {
             return false;
-        } else{ //jogada valida
+        } else { //jogada valida
             jogoVelha[linha][coluna] = sinal;
             return true;
         }
     }
-    
-        void imprimirTabuleiro() {
+
+    void imprimirTabuleiro() {
         for (int i = 0; i < jogoVelha.length; i++) {
             for (int j = 0; j < jogoVelha[i].length; j++) {
                 System.out.print(jogoVelha[i][j] + " | ");
             }
-            System.out.println( );
+            System.out.println();
         }
     }
-    
+
+    boolean verificarGanhador(char sinal) {
+        //verifica se há ganhador
+        if ((jogoVelha[0][0] == sinal && jogoVelha[0][1] == sinal && jogoVelha[0][2] == sinal) //linha 1
+                || (jogoVelha[1][0] == sinal && jogoVelha[1][1] == sinal && jogoVelha[1][2] == sinal) //linha 2
+                || (jogoVelha[2][0] == sinal && jogoVelha[2][1] == sinal && jogoVelha[2][2] == sinal) //linha 3
+                || (jogoVelha[0][0] == sinal && jogoVelha[1][0] == sinal && jogoVelha[2][0] == sinal) //coluna 1
+                || (jogoVelha[0][1] == sinal && jogoVelha[1][1] == sinal && jogoVelha[2][1] == sinal) //coluna 2
+                || (jogoVelha[0][2] == sinal && jogoVelha[1][2] == sinal && jogoVelha[2][2] == sinal) //coluna 3
+                || (jogoVelha[0][0] == sinal && jogoVelha[1][1] == sinal && jogoVelha[2][2] == sinal) //diagonal
+                || (jogoVelha[0][2] == sinal && jogoVelha[1][1] == sinal && jogoVelha[2][0] == sinal))//diagonal inversa        
+        {
+            return true;
+        }
+        return false;
+    }
+
 }
