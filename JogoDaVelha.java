f84fb1ae6c6d504b7b4f508365bed07e4887913a
Bruno Braga Mendonça <bruckbmrj@hotmail.com>
2017-04-18 01:29:57 -0300

    novas alterações POO
    criação de um construtor  e metodo para imprimir o tabuleiro no
    terminal.

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -10,8 +10,15 @@ package aula20_labs.JogoDaVelha;
  * @author Bruno
  */
 public class JogoDaVelha {
-    char[][] jogoVelha = new char[3][3];
-    int jogada = 1;
+    char[][] jogoVelha;
+    int jogada;
+
+    public JogoDaVelha() {
+       char[][] jogoVelha = new char[3][3];
+       jogada = 1;
+    }
+    
+    
     
     boolean validarJogada(int linha, int coluna, char sinal){
         if (jogoVelha[linha][coluna] == sinal || jogoVelha[linha][coluna] == 'O') {
@@ -21,4 +28,14 @@ public class JogoDaVelha {
             return true;
         }
     }
+    
+        void imprimirTabuleiro() {
+        for (int i = 0; i < jogoVelha.length; i++) {
+            for (int j = 0; j < jogoVelha[i].length; j++) {
+                System.out.print(jogoVelha[i][j] + " | ");
+            }
+            System.out.println( );
+        }
+    }
+    
 }
